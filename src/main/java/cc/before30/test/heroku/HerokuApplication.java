ab72ec3717514bc8fdf9c110d3aedcd86ac6ce12
Joseph Yang <before30@gmail.com>
2018-07-26 16:08:20 -0700

    baseline

---
+++ src/main/java/cc/before30/test/heroku/HerokuApplication.java
@@ -0,0 +1,30 @@
+package cc.before30.test.heroku;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Log4j2
+public class HerokuApplication {
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("hello");
+        return "world";
+    }
+
+    @GetMapping("/hi")
+    public String hi(@RequestParam("arg1") String arg1, @RequestParam("arg2") String arg2) {
+       log.info("hi {} {}", arg1, arg2);
+       return "hi" + arg1 + " " + arg2;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuApplication.class, args);
+    }
+}
